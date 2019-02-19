47ab607a9a8197b188b952f9cdb7d28be83998ba
Julio A. Mena Pinto <julio.mena@alnus.cl>
2019-02-19 01:27:47 -0300

    feat(PostalCode): Final version of getPostalCode

--- src/CchPostalcode/Cch.php
+++ src/CchPostalcode/Cch.php
@@ -2,10 +2,24 @@
 
 namespace CchPostalcode;
 
+use Goutte\Client;
+
 class Cch
 {
-    public static function getCode()
-    {
-        return 'Hello World, Composer!';
+    public static function getPostalCode($street, $number, $city) {
+        $code = 0;
+        try{
+          $_client = new Client();
+          $_crawler = $_client->request(
+            'GET',
+            'https://codigopostal.correos.cl/?calle='.urlencode($street).'&numero='.$number.'&comuna='.urlencode($city)
+          );
+
+          $code = trim($_crawler->filter('.tu_codigo')->first()->text());
+        } catch (\Throwable $ex){
+          $code = 0;
+        }
+
+        return $code;
     }
 }
